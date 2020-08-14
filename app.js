83099bd5436ef7d80e560a551a7fc56c006d1217
Aravind <aravindan7822@yahoo.com>
2020-08-14 22:06:07 +0530

    Data Handling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+// jshint:esversion6
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const data = require(__dirname+"/data.js");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -30,9 +31,19 @@ app.get("/sem", function (req, res) {
 
 app.post("/sem", function (req, res) {
     console.log(req.body.sem);
-    department_chosen = req.body.sem;
+    semester_chosen = req.body.sem;
+    res.redirect("/gpa");
 });
 
+app.get("/gpa",function(req,res){
+    let subjects;
+    data.getSubjects(department_chosen,semester_chosen).forEach(element => {
+        console.log(element)
+        subjects = element;
+    }); 
+    console.log(subjects);
+    res.render("/gpa",{subjects: Object.keys(subjects)});
+});
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
