7fdd1469be0dcd54fb8f7adadaaa7f793750a0a8
Aravind <aravindan7822@yahoo.com>
2020-08-14 20:07:35 +0530

    Logging Dept and Sem

--- app.js
+++ app.js
@@ -11,21 +11,26 @@ app.use(express.static("public"));
 
 const departments = ["MECH", "EEE", "ECE", "CSE", "IT", "FT", "CIVIL"];
 const semester = ["SEMESTER 1", "SEMESTER 2", "SEMESTER 3", "SEMESTER 4", "SEMESTER 5", "SEMESTER 6", "SEMESTER 7", "SEMESTER 8"];
+var department_chosen;
+var semester_chosen;
 
 app.get("/", function (req, res) {
     res.render("home", { departments: departments })
 });
 
-// app.post("/", function (req, res) {
-//     console.log(req.body.departments);
-// });
+app.post("/", function (req, res) {
+    console.log(req.body.department);
+    department_chosen = req.body.department;
+    res.redirect("/sem");
+});
 
 app.get("/sem", function (req, res) {
     res.render("sem", { semester: semester });
 });
 
 app.post("/sem", function (req, res) {
-    console.log(req.body.department);
+    console.log(req.body.sem);
+    department_chosen = req.body.sem;
 });
 
 app.listen(3000, function () {
