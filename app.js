984b80a4492e9803e695bbf798a487e2036ad41a
Esther Ruby V <51705296+EstherRuby21@users.noreply.github.com>
2020-08-15 20:58:57 +0530

    display gpa

--- app.js
+++ app.js
@@ -2,24 +2,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const data = require(__dirname+"/data.js");
+const data = require(__dirname + "/data.js");
 const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(session({
     secret: 'GPA Secret Key[-_-]',
     resave: false,
     saveUninitialized: true
-  }));
+}));
 
 const departments = ["MECH", "EEE", "ECE", "CSE", "IT", "FT", "CIVIL"];
 const semester = ["SEMESTER 1", "SEMESTER 2", "SEMESTER 3", "SEMESTER 4", "SEMESTER 5", "SEMESTER 6", "SEMESTER 7", "SEMESTER 8"];
 var department_chosen;
 var semester_chosen;
+let grade = [];
+let sub_obj;
+let subjects = [];
 
 app.get("/", function (req, res) {
     res.render("home", { departments: departments })
@@ -43,17 +46,24 @@ app.post("/sem", function (req, res) {
     res.redirect("/gpa");
 });
 
-app.get("/gpa",function(req,res){
-    let subjects=[];
-    const sub_obj =data.getSubjects(req.session.department_chosen,req.session.semester_chosen);
+app.get("/gpa", function (req, res) {
+    sub_obj = data.getSubjects(req.session.department_chosen, req.session.semester_chosen);
     for (let i = 1; i < sub_obj.length; i++) {
-         subjects.push( Object.keys(sub_obj[i]));
+        subjects.push(Object.keys(sub_obj[i]));
     }
-    // console.log("subjects"+subjects);
-    res.render("gpa",{subjects: subjects});
+    res.render("gpa", { subjects: subjects });
 });
-app.post("/gpa",function (req, res){
-    console.log(req.body.subject);
+app.post("/gpa", function (req, res) {
+    grade = req.body.subject;
+    let sum = 0;
+    let total = 0;
+    for (let i = 0; i < grade.length; i++) {
+        let val = parseInt(Object.values(sub_obj[i])[0]);
+        sum = sum + parseInt(grade[i] * val);
+        total += val;
+    }
+    let gpa = sum / total;
+    res.send("<h1>" + gpa + "</h1>");
 });
 app.listen(3000, function () {
     console.log("Server started on port 3000");
